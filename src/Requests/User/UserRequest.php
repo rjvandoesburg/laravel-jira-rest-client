ec11aedea03b4892b60c6fede84fa21aa3ddfd7b
Jelle Witteveen <jelle@wittev1.com>
2021-01-20 10:45:59 +0100

    Issue 26: expand UserRequest with an extra method to support backwards compatibility

--- src/Requests/User/UserRequest.php
+++ src/Requests/User/UserRequest.php
@@ -26,7 +26,8 @@ class UserRequest extends AbstractRequest
     }
 
     /**
-     * Returns a list of users that match the search string and/or property.
+     * Returns a list of users that match the search string and/or property and
+     * are assignable to projects or issues.
      * This resource cannot be accessed anonymously.
      *
      * @see https://developer.atlassian.com/cloud/jira/platform/rest/v3/#api-rest-api-3-users-search-get
@@ -40,6 +41,25 @@ class UserRequest extends AbstractRequest
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function search($parameters)
+    {
+        return $this->execute('get', 'user/search', $parameters);
+    }
+
+    /**
+     * Returns a list of users that match the search string and/or property
+     * This resource cannot be accessed anonymously.
+     *
+     * @see https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-user-search/#api-rest-api-3-user-search-get
+     *
+     * @param  array|\\Illuminate\\Contracts\\Support\\Arrayable  $parameters
+     *
+     * @return \GuzzleHttp\Psr7\Response
+     * @throws \Atlassian\JiraRest\Exceptions\JiraClientException
+     * @throws \Atlassian\JiraRest\Exceptions\JiraNotFoundException
+     * @throws \Atlassian\JiraRest\Exceptions\JiraUnauthorizedException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function searchAll($parameters)
     {
         return $this->execute('get', 'users/search', $parameters);
     }
