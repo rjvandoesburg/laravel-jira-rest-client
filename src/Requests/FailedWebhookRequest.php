1e9f689436cbbc66225cf6939175f33ae5c785e9
dion213 <d.nijssen@creacoon.nl>
2021-05-28 14:28:55 +0200

    added failed webhooks api call

---
+++ src/Requests/FailedWebhookRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Atlassian\JiraRest\Requests;
+
+class FailedWebhookRequest extends AbstractRequest
+{
+    /**
+     * Get failed webhooks.
+     *
+     * @see https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-webhooks/#api-rest-api-3-webhook-failed-get
+     *
+     * @param  \Atlassian\JiraRest\Requests\Issue\Parameters\Transitions\DoTransitionsParameters|array
+     *
+     * @return \GuzzleHttp\Psr7\Response
+     * @throws \Atlassian\JiraRest\Exceptions\JiraClientException
+     * @throws \Atlassian\JiraRest\Exceptions\JiraNotFoundException
+     * @throws \Atlassian\JiraRest\Exceptions\JiraUnauthorizedException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function get($parameters)
+    {
+        return $this->execute('get', 'webhook/failed', $parameters);
+    }
+}
